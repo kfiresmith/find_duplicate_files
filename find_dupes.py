10b8424bda8754503f68430b417fad3376012b3f
Kodiak Firesmith <kodiak@firesmith.org>
2017-07-02 21:48:33 -0400

    Working version.  Needs a lot of refinement.

--- find_dupes.py
+++ find_dupes.py
@@ -1,10 +1,11 @@
 import os
 import hashlib
+    # TODO: only hash the start of each large file file; VM disks eat all the ram!
 
 
 
 try:
-    os.chdir("/home/freeos/logs")
+    os.chdir("/")
 except PermissionError:
     print("Couldn't enter target directory")
 
@@ -16,6 +17,8 @@ for root, dirs, files in os.walk(str(os.getcwd()), "*"):
             itempath = os.path.join(root, name)
             try:
                 itemsum = hashlib.sha1(open(itempath, 'rb').read()).hexdigest()
+                if itemsum in filesums:
+                    print("Found duplicate file checksum: " + itemsum + " in both: " + itempath + " and in: " + filesums[itemsum])
 
                 # Add new item to dictionary
                 filesums[itemsum] = itempath
@@ -23,12 +26,13 @@ for root, dirs, files in os.walk(str(os.getcwd()), "*"):
             except (FileNotFoundError, OSError):
                 print("Skipping problematic file: " + itempath)
                 pass
-            #if itemsum in filesums:
-            #    print("Found duplicate file checksum " + itemsum + "in both " + itempath " and in " filesums[itemsum])
+            except MemoryError:
+                print("Whoops ate all the RAM...")
+                pass
         # Accept that there may be files we can't access
         except PermissionError:
             print("Hit permissions problem on: " + itempath)
             pass
 
-print(filesums)
+#print(filesums) # Debug for small tests
 os._exit(0)
